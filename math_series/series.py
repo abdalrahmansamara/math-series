6e49bc1e67da98b42f29c2ae16043af7f03ca5f4
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-06-08 19:46:30 +0300

    fix the last issue

--- math_series/series.py
+++ math_series/series.py
@@ -1,29 +1,23 @@
 def fibonacci(n):
   if(type(n) != int):
     return 'not an integer'
-  if (n <= 1):
-    return n
-  return fibonacci(n-1) + fibonacci(n-2)
+  return sum_series(n, 0, 1)
 
 def lucas(n):
   if(type(n) != int):
     return 'not an integer'
-  if (n == 0):
-    return 2
-  elif (n == 1):
-    return n
-  return lucas(n-1) + lucas(n-2)
+  return sum_series(n, 2, 1)
+  # if (n == 0):
+  #   return 2
+  # elif (n == 1):
+  #   return n
+  # return lucas(n-1) + lucas(n-2)
 
 def sum_series(n,b=0,c=1):
   if(type(n) != int or type(b) != int or type(c) != int):
     return 'not an integer'
-  if( b==0 and c == 1):
-    return fibonacci(n)
-  elif (b == 2 and c == 1):
-    return lucas(n)
-  else:
-    if (n == 0):
-      return b
-    elif (n == 1):
-      return c
-    
+  if (n == 0):
+    return b
+  elif (n == 1):
+    return c
+  return sum_series(n-1,b,c) + sum_series(n-2,b,c)
